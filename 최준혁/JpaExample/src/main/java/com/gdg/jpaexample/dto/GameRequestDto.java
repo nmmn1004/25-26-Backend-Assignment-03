de20ce7bf8afc884aa47ffd8078b5ed07d82f679
nmmn1004 <jjj4120@gmail.com>
2025-11-02 01:44:00 +0900

    Add: Upload img postman result

--- 최준혁/JpaExample/src/main/java/com/gdg/jpaexample/dto/GameRequestDto.java
+++ 최준혁/JpaExample/src/main/java/com/gdg/jpaexample/dto/GameRequestDto.java
@@ -2,9 +2,12 @@ package com.gdg.jpaexample.dto;
 
 import jakarta.annotation.Nullable;
 import lombok.Getter;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Getter
 public class GameRequestDto {
+    @NotNull
     private Long playerId;
+    @NotNull
     private Long bettingChips;
 }
